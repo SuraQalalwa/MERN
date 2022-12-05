0364e1d47107cf5d8c493cf7eaf9406f7ce386d1
Sura Qalalwa <suraqalalwa@outlook.com>
2022-12-05 22:06:23 +0200

    commit

---
+++ React/First_React_JSX/src/componants/index.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class Dojo extends Component {    
+        render() {        
+            return( 
+            <div>
+                <h1 className="my-class">Hello Dojo!</h1>
+                <h4 className="my-class">Things I need to do:</h4>
+                <ul className="my-class">
+                    <li>Learn React</li>
+                    <li>Climb Mt Evereset</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>            
+        )
+            ;    
+        }
+    }
+export default Dojo;
