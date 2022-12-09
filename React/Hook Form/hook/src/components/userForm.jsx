2d17c8f22137567f22a9c2724aeb3dd1d5f98d52
Sura Qalalwa <suraqalalwa@outlook.com>
2022-12-09 22:24:13 +0200

    commit

--- React/Hook Form/hook/src/components/userForm.jsx
+++ React/Hook Form/hook/src/components/userForm.jsx
@@ -66,64 +66,64 @@ return(
     <>
     <div>
 <form onSubmit={ (e) => e.preventDefault() }>
-<div>
-<label>First Name: </label> 
-<input type="text"  onChange={ handlefirstName } />
-        {
-                    firstNameError ?
-                    <p style={{color:'red'}}>{ firstNameError }</p> :
-                    ''
-                }
-</div>
-<div>
-    <label>Last Name: </label> 
-<input type="text" onChange={handlelastName}  />
-{
-                    lastNameError ?
-                    <p style={{color:'red'}}>{ lastNameError }</p> :
-                    ''
-                }
-</div>
-<div>
-<label>Email Address: </label> 
-<input type="text" onChange={handleEmail}/>
-{
-    emailError?
-    <p style={{color:'red'}}>{emailError}</p>:
-    ''
-}
-</div>
-<div>
-<label>Password: </label>
-<input type="password" onChange={handlepassword} />
-{
-    passwordError?
-    <p style={{color:'red'}}>{passwordError}</p>:
-    ''
-}
-</div>
-<div>
-<label>Confirm Password: </label>
-<input type="password" onChange={handleconfirmpassword} />
-{
-    confirmpasswordError?
-    <p style={{color:'red'}}>{confirmpasswordError}</p>:
-    ''
-}
-</div>
+    <div>
+        <label>First Name: </label> 
+        <input type="text"  onChange={ handlefirstName } />
+            {
+                        firstNameError ?
+                        <p style={{color:'red'}}>{ firstNameError }</p> :
+                        ''
+                    }
+    </div>
+    <div>
+        <label>Last Name: </label> 
+        <input type="text" onChange={handlelastName}  />
+    {
+                        lastNameError ?
+                        <p style={{color:'red'}}>{ lastNameError }</p> :
+                        ''
+                    }
+    </div>
+    <div>
+        <label>Email Address: </label> 
+        <input type="text" onChange={handleEmail}/>
+    {
+        emailError?
+        <p style={{color:'red'}}>{emailError}</p>:
+        ''
+    }
+    </div>
+    <div>
+        <label>Password: </label>
+        <input type="password" onChange={handlepassword} />
+    {
+        passwordError?
+        <p style={{color:'red'}}>{passwordError}</p>:
+        ''
+    }
+    </div>
+    <div>
+        <label>Confirm Password: </label>
+        <input type="password" onChange={handleconfirmpassword} />
+    {
+        confirmpasswordError?
+        <p style={{color:'red'}}>{confirmpasswordError}</p>:
+        ''
+    }
+    </div>
 </form>
-</div>
+    </div>
 
-<div>
-    <h2>Your Form Data</h2>
-<p>First Name :{firstName}</p>
-<p>Last Name :{lastName}</p>
-<p>Email: {email}</p>
-<p>Password:{password.type}</p>
-<p>Confirm Password:{confirmpassword.type}</p>
-</div>
-</>
-);
-};
+    <div>
+        <h2>Your Form Data</h2>
+        <p>First Name :{firstName}</p>
+        <p>Last Name :{lastName}</p>
+        <p>Email: {email}</p>
+        <p>Password:{password.type}</p>
+        <p>Confirm Password:{confirmpassword.type}</p>
+    </div>
+    </>
+    );
+    };
 
 export default UserForm;
