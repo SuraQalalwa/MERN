50636c50703e147318863f209a0d7752e20652d7
Sura Qalalwa <suraqalalwa@outlook.com>
2022-12-11 22:34:32 +0200

    tab commit

--- React/Tabs/tabs/src/components/TabDisplay.js
+++ React/Tabs/tabs/src/components/TabDisplay.js
@@ -4,7 +4,7 @@ import React from 'react';
 const TabDisplay = (props) => {
     
 return (
-<pre><textarea value={"this shows tab"+props.tabs.id}></textarea></pre>
+<pre><textarea value={props.tabs === ""?"":"this shows tab"+props.tabs.id }></textarea></pre>
 )
 }
 
