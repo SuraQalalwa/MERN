64b297ece9b0a43912df4e4e0e4a6eed4281fd9f
Sura Qalalwa <suraqalalwa@outlook.com>
2022-12-16 18:39:18 +0200

    updateRoute commit

--- React/Jokes/Jokes/server/controllers/jokes.controllers.js
+++ React/Jokes/Jokes/server/controllers/jokes.controllers.js
@@ -11,16 +11,11 @@ module.exports.createJokes=(req,res)=>{
         .then(newJokes=> res.json({joke: newJokes}))
         .catch((err)=> res.json({message: "Something went wrong", error:err}))
 };
-
-module.exports.updateJoke=(req,res)=>{
-    Jokes.findOneAndUpdate(
-        {_id:req.params.id},
-        req.body,
-        {new:true, runValidators: true}
-        .then(updateJoke=>res.json({joke:updateJoke}))
-        .catch(err=>res.json({message: "Somthing went wrong",error:err}))
-    )};
-
+module.exports.updatejoke = (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .then(updatedJoke => res.json({ joke: updatedJoke }))
+        .catch(err => res.json({ message: "Something went wrong", error: err }));
+    };
 module.exports.deletejoke=(req,res)=>{
     Jokes.findOneAndRemove({_id:req.params.id})
         .then(result=>res.json({result: result}))
--- React/Jokes/Jokes/server/routes/jokes.routes.js
+++ React/Jokes/Jokes/server/routes/jokes.routes.js
@@ -3,10 +3,10 @@ const JokesController = require('../controllers/jokes.controllers');
 module.exports = app => {
     app.get("/api/jokes", JokesController.findAlljokes);
     app.get("/api/jokes/:id", JokesController.getjoke);
-    app.put("/api/jokes/update/:id", JokesController.updateJoke);
+    app.put("/api/jokes/update/:id", JokesController.updatejoke);
     app.post("/api/jokes/new",JokesController.createJokes);
     app.delete("/api/jokes/delete/:id", JokesController.deletejoke)
-}
+};
 
     
     
